d807436738e2eb34b915a7844c1dcbd0b73d4049
Lucas Felipe Galvão Silva <l17.lucas@gmail.com>
2023-09-21 22:58:04 +0000

    indentação para lint não chiar

--- Trabalho_4_Und_1/main.py
+++ Trabalho_4_Und_1/main.py
@@ -8,6 +8,8 @@ laptop_faixa = inventario.find_laptops_in_price_range('990', '1000')
 
 print(len(laptop_faixa))
 
-laptop_barato = inventario.find_cheapest_laptop_with_specifications(['Apple', 'Ultrabook', '13.3', '8GB', '256GB SSD'])
+laptop_barato = inventario.find_cheapest_laptop_with_specifications(
+        ['Apple', 'Ultrabook', '13.3', '8GB', '256GB SSD']
+    )
 
 print(laptop_barato)

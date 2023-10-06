3fe06b64fb857f5a67b7a2e9acff5c0142703932
Lucas Felipe Galvão Silva <l17.lucas@gmail.com>
2023-10-06 22:11:11 +0000

    trabalho 2 unidade 1

--- Trabalho_4_Und_1/main2.py
+++ Trabalho_4_Und_1/main2.py
@@ -1,4 +1,3 @@
-"""programa principal 2"""
 import time
 import matplotlib.pyplot as plt
 
@@ -6,33 +5,40 @@ import matplotlib.pyplot as plt
 from inventory import Inventory  # Certifique-se de substituir 'inventory' pelo nome do seu arquivo
 
 # Crie uma instância da classe Inventory com o arquivo CSV desejado
-CSV_FILE = "laptops.csv"  # Substitua pelo nome do seu arquivo CSV
-inventory = Inventory(CSV_FILE)
+csv_filename = "laptops.csv"  # Substitua pelo nome do seu arquivo CSV
+inventory = Inventory(csv_filename)
 
 # Tamanho total do inventário (1303 produtos no seu caso)
-INVENTORY_SIZE = len(inventory.rows)
+total_inventory_size = len(inventory.rows)
 
-# Defina as faixas de tamanho desejadas
-# Por exemplo, você pode definir faixas de tamanho de 100 a 1000 em incrementos de 100
-faixas_de_tamanho = list(range(100, 1100, 100))
+# Tamanho inicial da primeira faixa
+tamanho_inicial_da_faixa = 100
 
-# Lista para armazenar os tempos de execução para cada faixa
+# Incremento de tamanho da faixa (espaçamento entre as faixas)
+incremento_da_faixa = 50  # Você pode ajustar isso para controlar o aumento
+
+# Inicialize a lista de tamanhos do inventário com o tamanho inicial
+tamanhos_do_inventario = [tamanho_inicial_da_faixa]
+
+# Adicione tamanhos adicionais com base no incremento até incluir o tamanho total do inventário
+while tamanhos_do_inventario[-1] + incremento_da_faixa <= total_inventory_size:
+    proximo_tamanho = tamanhos_do_inventario[-1] + incremento_da_faixa
+    tamanhos_do_inventario.append(proximo_tamanho)
+
+# Lista para armazenar os tempos de execução para cada tamanho
 tempos_de_execucao = []
 
-for tamanho_da_faixa in faixas_de_tamanho:
-    # Calcule o tamanho do inventário para a faixa atual
-    tamanho_da_faixa = min(tamanho_da_faixa, INVENTORY_SIZE)
-
-    # Configure o tamanho do inventário para a faixa atual
-    inventario_da_faixa = inventory.rows[:tamanho_da_faixa]
+for tamanho_do_inventario in tamanhos_do_inventario:
+    # Configure o tamanho do inventário para o tamanho atual
+    inventario_da_faixa = inventory.rows[:tamanho_do_inventario]
 
     # Mede o tempo de execução das funções para esta faixa
     start_time = time.time()
-
+    
     # Realize as operações que você deseja medir aqui
     # Por exemplo:
-    # inventory.find_laptops_in_price_range(900, 1100)
-    inventory.find_cheapest_laptop_with_specifications(["Intel i7", "16GB"])
+    inventory.find_laptops_in_price_range(900, 1100)
+    # inventory.find_cheapest_laptop_with_specifications(["Intel i7", "16GB"])
 
     end_time = time.time()
     tempo_de_execucao = end_time - start_time
@@ -41,9 +47,9 @@ for tamanho_da_faixa in faixas_de_tamanho:
     tempos_de_execucao.append(tempo_de_execucao)
 
 # Crie o gráfico de linha para visualizar os tempos de execução
-plt.plot(faixas_de_tamanho, tempos_de_execucao, marker='o')
-plt.xlabel("Tamanho da Faixa")
+plt.plot(tamanhos_do_inventario, tempos_de_execucao, marker='o')
+plt.xlabel("Tamanho do Inventário")
 plt.ylabel("Tempo de Execução (segundos)")
-plt.title("Tempo de Execução para Diferentes Faixas de Tamanho do Inventário")
+plt.title("Tempo de Execução para Diferentes Tamanhos do Inventário (Com Crescimento Linear)")
 plt.grid(True)
-plt.savefig('grafico2.png')
+plt.savefig('grafico5.png')

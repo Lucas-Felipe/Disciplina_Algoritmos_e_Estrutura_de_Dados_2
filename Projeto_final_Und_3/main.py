aaaada468ff1c7da7677a691bf2d87a4807019a0
Lucas Felipe Galvão Silva <l17.lucas@gmail.com>
2023-12-16 11:36:57 +0000

    retirando código que não irá ser utilizado

--- Projeto_final_Und_3/main.py
+++ Projeto_final_Und_3/main.py
@@ -1,12 +1,6 @@
 import matplotlib.pyplot as plt
 import osmnx as ox
 
-# Carrega o grafo da área a partir do arquivo .osm
-# graph = ox.graph_from_xml('map.osm', simplify=False)
-
-# Plota o grafo
-# ox.plot_graph(ox.project_graph(graph))
-
 # Seu código para gerar o gráfico com o OSMnx
 place_name = "Santa Cruz, RN"
 graph = ox.graph_from_place(place_name, network_type='all')
@@ -20,3 +14,4 @@ num_edges = ox.utils_graph.graph_to_gdfs(graph, nodes=False).shape[0]
 
 print(f"Número de Nós: {num_nodes}")
 print(f"Número de Arestas: {num_edges}")
+
